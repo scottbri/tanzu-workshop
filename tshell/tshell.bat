08bdebbdf8d20e8923b05504e4edbb28d5f8f76c
scott brightwell <scott@brightwell.org>
2021-09-18 11:33:48 -0500

    reorg

---
+++ tshell/tshell.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set TEMPKUBE=temp-kube-config.yaml
+set YAMLURL="https://raw.githubusercontent.com/scottbri/tanzu-workshop/main/tshell.yaml"
+
+kubectl config view --raw > %TEMPKUBE%
+
+echo "Ensure tshell namespace and pod are created on cluster"
+kubectl create -f "%YAMLURL%"
+
+echo "When tshell pod is Running.  Hit <ctrl>-c to continue"
+kubectl -n tshell get pod/tshell -w
+
+echo "Copy KUBECONFIG to tshell"
+kubectl cp %TEMPKUBE% tshell/tshell:/home/tanzu/.kube/config
+
+echo "Launch bash in the tshell"
+echo "Command reference: kubectl -n tshell exec tshell -it -- bash"
+kubectl -n tshell exec tshell -it -- bash
+
+rm %TEMPKUBE%
