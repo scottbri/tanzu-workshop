b959be3daf6a271e822e368e6f1f3a7ce263b0c3
Scott Brightwell <scott@brightwell.org>
2021-12-13 23:52:03 -0600

    Update create-cluster.sh

--- create-cluster/create-cluster.sh
+++ create-cluster/create-cluster.sh
@@ -2,12 +2,6 @@ set -e
 
 CLUSTER=${1:-lab${RANDOM}}
 
-if [[ x${VSPHERE_CONTROL_PLANE_ENDPOINT} == "x" ]]
-then
-   echo "ERROR:  Set VSPHERE_CONTROL_PLANE_ENDPOINT with the IP address of the KubeVIP for the cluster"
-   exit 1
-fi
-
 
 echo "Using cluster name ${CLUSTER} with IP address ${VSPHERE_CONTROL_PLANE_ENDPOINT}"
 read -p "Do you want to continue? " -n 1 -r
@@ -17,8 +11,7 @@ then
 #	PUBLIC_SUBNET_ID=subnet-084ef6790faa5b919
 
 	cp -r template-small ${CLUSTER}
-	#mv ${CLUSTER}/config-cluster.yaml ${CLUSTER}/config-${CLUSTER}.yaml
-	cat ${CLUSTER}/config-cluster.yaml |  sed -e "s/CHANGEME_ENDPOINT/$VSPHERE_CONTROL_PLANE_ENDPOINT/"  > ${CLUSTER}/config-${CLUSTER}.yaml
+	mv ${CLUSTER}/config-cluster.yaml ${CLUSTER}/config-${CLUSTER}.yaml
 
 #	aws ec2 create-tags --resources ${PUBLIC_SUBNET_ID} --tags Key=kubernetes.io/cluster/${CLUSTER},Value=shared
 
